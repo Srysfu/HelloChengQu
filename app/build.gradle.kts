a68f5a713de5d1246824587e2cfa3055a7dc3a26
Srysfu <68031257+Srysfu@users.noreply.github.com>
2026-08-14 12:15:20 +0800

    fix: update compileSdk, versionCode/Name, add backdrop

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.srysfu.nokey.hook"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
